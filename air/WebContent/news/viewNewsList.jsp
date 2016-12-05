f6e27ee445ce2d78bef4bbaaca6e527b16a748f0
bitUser <bitUser@bitacademy>
2016-12-05 11:30:13 +0900

    1205nn

--- air/WebContent/news/viewNewsList.jsp
+++ air/WebContent/news/viewNewsList.jsp
@@ -10,51 +10,69 @@
 </head>
 <body>
  <h1>국가 검색</h1>
-<%-- 	<form action="/student1/getNewsList.do" method="post">
-		검색어<input type="text"  name="keyword"/>
-		<input type="submit"  value="검색"/>
-	</form>--%>
-	
-	<c:choose>
-		<c:when test="${ITEM!=null}">
-			<table width="600" align="center" border="1">
-				<tr>
-					<td>번호</td>
-					<td>제목</td>
-					<td>오리지날링크</td>
-					
-					<td>설명</td>
-					<td>발행일</td>
-				
-					
-				</tr>
-				
-				<c:forEach items="${ITEM}" var="item" 
-				 varStatus="i">
-				 <tr>
-				 	<td>${i.count}</td>
-				 	<td>${item.title}</td>
-				 	<td><a href="${item.originallink}">뉴스자세히보기</a></td>
-				 	
-				 	
-				 
-				 	<td>
-				 	
-				 	${item.description}
-				 	
-				 	</td>
-				 	<td>${item.pubDate}</td>
-				 	
-				 		
-				 </tr>	
-				 </c:forEach>	
-				 
-				 	
-			</table>
-		</c:when>
-		<c:otherwise>
-			검색 결과가 없습니다.
-		</c:otherwise>
-	</c:choose>
+<%--    <form action="/student1/getNewsList.do" method="post">
+      검색어<input type="text"  name="keyword"/>
+      <input type="submit"  value="검색"/>
+   </form>--%>
+   
+   <c:choose>
+      <c:when test="${ITEM!=null}">
+         <table width="600" align="center" border="1">
+            <tr>
+               <td>번호</td>
+               <td>제목</td>
+               <td>오리지날링크</td>
+               
+               <td>설명</td>
+               <td>발행일</td>
+            
+               
+            </tr>
+            
+            <c:forEach items="${ITEM}" var="item" 
+             varStatus="i">
+             <tr>
+                <td>${i.count}</td>
+                <td>${item.title}</td>
+                <td><a href="${item.originallink}">뉴스자세히보기</a></td>
+                
+                
+             
+                <td>
+                
+                ${item.description}
+                
+                </td>
+                <td>${item.pubDate}</td>
+                
+                   
+             </tr>   
+             </c:forEach>   
+             <c:forEach items="${ITEM2}" var="item2" 
+             varStatus="i2">
+             <tr>
+                <td>${i2.count}</td>
+                <td>${item2.title}</td>
+                <td><a href="${item2.originallink}">뉴스자세히보기</a></td>
+                
+                
+             
+                <td>
+                
+                ${item2.description}
+                
+                </td>
+                <td>${item2.pubDate}</td>
+                
+                   
+             </tr>   
+             </c:forEach>   
+                
+         </table>
+      </c:when>
+      <c:otherwise>
+         검색 결과가 없습니다.
+      </c:otherwise>
+   </c:choose>
 </body>
 </html>
--- air/src/bitcom/air/info/controller/Naver.java
+++ air/src/bitcom/air/info/controller/Naver.java
@@ -20,58 +20,23 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 @Controller
 public class Naver {
-	@Resource
-	ModelAndView mav;
-	@RequestMapping("/getNewsListForm.do")
-	public ModelAndView getNewsListForm(){
-		mav.setViewName("/news/viewNewsList.jsp");
-		return mav;
-		}
-	@RequestMapping("/getNewsList.do")
-	public ModelAndView getNewsList() throws Exception {
-		// TODO Auto-generated method stub
-		 String clientId = "eHYOfpZR4LeJ0nWtLYfl";//���ø����̼� Ŭ���̾�Ʈ ���̵�";
-	        String clientSecret = "k6Ku9x8l_O";//���ø����̼� Ŭ���̾�Ʈ ��ũ����";
-	        
-	            String text = URLEncoder.encode("트럼프","UTF-8");
-	            String apiURL = "https://openapi.naver.com/v1/search/news.json?query="+ text;
-	            URL url = new URL(apiURL);
-	            HttpURLConnection con = (HttpURLConnection)url.openConnection();
-	            con.setRequestMethod("GET");
-	            con.setRequestProperty("X-Naver-Client-Id", clientId);
-	            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
-	            int responseCode = con.getResponseCode();
-	            BufferedReader br;
-	            if(responseCode==200) { // ���� ȣ��
-	                br = new BufferedReader(new InputStreamReader(con.getInputStream(),"UTF-8"));
-	            } else {  // ���� �߻�
-	                br = new BufferedReader(new InputStreamReader(con.getErrorStream(),"UTF-8"));
-	            }
-	            String inputLine;
-	            StringBuffer response = new StringBuffer();
-	            while ((inputLine = br.readLine()) != null) {
-	                response.append(inputLine);
-	            }
-	            
-	         
-	            JsonObject root = new JsonParser().parse(response.toString()).getAsJsonObject();
-	 		   
-	       
-	            JsonArray subnode=root.getAsJsonArray("items");
-	         
-	 		  Gson gson = new Gson();
-	 		  
-	 		 ArrayList <Item>itemList=new ArrayList<Item>();
-			   for(int i=0;i<subnode.size();i++){
-				   Item item=gson.fromJson(subnode.get(i),Item.class);
-				   itemList.add(item);
-			   }
-			   
-			   mav.addObject("ITEM",itemList);
-			   mav.setViewName("/news/viewNewsList.jsp");
-			   return mav;
-	        } 
+   @Resource
+   ModelAndView mav;
+   @RequestMapping("/getNewsListForm.do")
+   public ModelAndView getNewsListForm(){
+      mav.setViewName("/news/viewNewsList.jsp");
+      return mav;
+      }
+   @RequestMapping("/getNewsList.do")
+   public ModelAndView getNewsList() throws Exception {
+      // TODO Auto-generated method stub
+          ArrayList<Item> itemList = getNews.get("트럼프");
+            mav.addObject("ITEM",itemList);
+            ArrayList<Item> itemList2 = getNews.get("국제신문");
+            mav.addObject("ITEM2",itemList2);
+            mav.setViewName("/news/viewNewsList.jsp");
+            return mav;
+           } 
 
-	}
-
+   }
 
